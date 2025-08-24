3afc951ac26bc0098aa87939f57f45ebaa517a61
Aditya Ojha <ojha8aditya@gmail.com>
2025-08-24 09:36:48 +0530

    Add files via upload
    This program obtains two numbers from the user and displays their difference.

---
+++ SUBTRACT.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("Enter first number: ");
+	scanf("%d",&a);
+	printf("Enter second number: ");
+	scanf("%d",&b);
+	c=a-b;
+	printf("The difference of two numbers is: %d",c);
+	getch();
+}
