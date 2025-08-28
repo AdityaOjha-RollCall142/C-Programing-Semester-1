23ea7a080c2c3e287fb435f8adb241878451428a
Aditya Ojha <ojha8aditya@gmail.com>
2025-08-28 21:58:29 +0530

    Update AdityaOjha_Div2H4ICT_RollNo._Prog5.C

--- AdityaOjha_Div2H4ICT_RollNo._Prog5.C
+++ AdityaOjha_Div2H4ICT_RollNo._Prog5.C
@@ -3,18 +3,19 @@
 
 void main()
 {
-	int a,b,c;
+	float a,b,c;
 	printf("Enter first number: ");
-	scanf("%d",&a);
+	scanf("%2f",&a);
 	printf("Enter second number: ");
-	scanf("%d",&b);
+	scanf("%2f",&b);
 	c=a+b;
-	printf("The sum of the numbers is: %d\n",c);
+	printf("The sum of the numbers is: %2f\n",c);
 	c=a-b;
-	printf("The difference of the numbers is: %d\n",c);
+	printf("The difference of the numbers is: %2f\n",c);
 	c=a*b;
-	printf("The product of the numbers is: %d\n",c);
+	printf("The product of the numbers is: %2f\n",c);
 	c=a/b;
-	printf("The quotient of the numbers is: %d\n",c);
+	printf("The quotient of the numbers is: %2f\n",c);
 	getch();
 }
+
