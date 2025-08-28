49ecaded001c56d4470f05bffb151ca862fde756
Aditya Ojha <ojha8aditya@gmail.com>
2025-08-28 22:01:48 +0530

    Update AdityaOjha_Div2H4ICT_RollNo._Progr11.C

--- AdityaOjha_Div2H4ICT_RollNo._Progr11.C
+++ AdityaOjha_Div2H4ICT_RollNo._Progr11.C
@@ -2,12 +2,13 @@
 #include <conio.h>
 void main()
 {
-	int grams,kg,remgrams;
+	float grams,kg,remgrams;
 	printf("ENTER GRAMS: ");
-	scanf("%d",&grams);
+	scanf("%2f",&grams);
 	kg=grams/1000;
 	remgrams=grams%1000;
-	printf("Given grams in kgs is %d",kg);
-	printf(".%d",remgrams);
+	printf("Given grams in kgs is %2f",kg);
+	printf(".%2f",remgrams);
 	getch();
 }
+
