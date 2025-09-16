6079a94fe4f60c949cc07ee5ffc7243142ac447a
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-16 23:40:39 +0530

    Update AdityaOjha_Div3H5ICT_RollNo.202_Progr23.C

--- AdityaOjha_Div3H5ICT_RollNo.202_Progr23.C
+++ AdityaOjha_Div3H5ICT_RollNo.202_Progr23.C
@@ -2,10 +2,12 @@
 #include <conio.h>
 void main()
 {
-	int avg,s1,s2,s3;
+	int avg,s1,s2,s3,t;
 	printf("Enter the marks for the three subjects: ");
 	scanf("%d %d %d",&s1, &s2, &s3);
 	avg=(s1+s2+s3)/3;
-	printf("Average marks of the three subjects are: %d",avg);
+	t=s1+s2+s3;
+	printf("Average marks of the three subjects are: %d and the total is %d",avg,t);
 	getch();
 }
+
