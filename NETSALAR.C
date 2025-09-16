4a7dee363985ebf0498456a67debb4de2e80c322
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-16 17:11:59 +0530

    Add files via upload
    This program calculates net salary.

---
+++ NETSALAR.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int ns,gs,all,ded;
+	printf("Enter net salary: ");
+	scanf("%d",&gs);
+	all=0.1*gs;
+	ded=0.03*gs;
+	ns=gs+all-ded;
+	printf("Net salary is: %d",ns);
+}
